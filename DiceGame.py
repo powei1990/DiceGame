e2754563474bcb50279ed7c0574ce01246ac278f
Powei <powei1990@gmail.com>
2019-07-01 22:36:57 +0800

    Update DiceGame.py

--- DiceGame.py
+++ DiceGame.py
@@ -1,12 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[72]:
 
 
 import random
-banker=(random.sample(range(1, 7), 4))
-banker
+banker=roll()
+player=roll()
+#print("banker")
+#print(banker)
+#print("player")
+#print(player)
+
+
+# In[68]:
+
+
+def roll():
+    result=[]
+    for i in range(4):
+        result.append(random.randrange(1, 7))
+    check(result)
+    return result
+
+
+# In[65]:
+
+
+def check(result):
+    for i in range(6):
+        if result.count(i)==2:
+            print("%i confirm"%i)
+            isconfirm=True
+            print(result)
+
+           #calculate(result,i)
+            break
+        else:
+            isconfirm=False
+    if isconfirm==False:
+        print("reroll")
+        roll()
+
+
+# In[2]:
+
+
+def calculate(result,i):
+    temp[0:4]=result[0:4] 
+    temp.remove(i)
+    temp.remove(i)
+        
+
+
+# In[73]:
+
+
+player
 
 
 # In[ ]:
