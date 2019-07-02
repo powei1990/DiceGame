952a8df6ca48be7e59a108bf69ac38cae61b0dfe
Powei <powei1990@gmail.com>
2019-07-02 23:06:16 +0800

    Update DiceGame.py
    多人遊戲

--- DiceGame.py
+++ DiceGame.py
@@ -1,28 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import random
 
+
+# In[ ]:
+
+
 def roll():
-    result=[]
-    for i in range(4):
-        result.append(random.randrange(1, 7))
-    check(result)
-    return result
+    score = 0
+    while not score:
+        point=[random.choice(dice) for a in range(4)]
+        print(point)
+        score=check(point)
+        #print(score)
+    return score
 
-def check(result):
+
+# In[ ]:
+
+
+def check(point):
     for i in range(6):
-        if result.count(i)==2:
-            print("%i confirm"%i)
+        if point.count(i)==6:
+            score=18
             isconfirm=True
-            print(result)
-
-           #calculate(result,i)
+            return score
+        elif point.count(i)==2:
+            #print("%i confirm"%i)
+            score=sum(list(filter(lambda n:n!=i,point)))
+            #print(score)
+            isconfirm=True
+            #print(point)
+            return score
             break
-        else:
-            isconfirm=False
-    if isconfirm==False:
-        print("reroll")
-        roll()
+    return 0
 
 
-banker=roll()
-player=roll()
+# In[ ]:
+
+
+def compare(matrx):
+    
+        
+
+
+# In[ ]:
+
+
+def endgame():
+
+
+# In[ ]:
+
+
+def game_start(matrx,num_of_player):
+    for i in range(0,num_of_player):
+        matrx[i][0]=roll()
+        print("玩家%i"%i)
+        print("%s分"%matrx[i][0])
+    #print(matrx)
+
+
+# In[ ]:
+
+
+print ("***擲骰子遊戲***")
+dice = [i for i in range(1,7)]
+num_of_player=int(input("多少玩家"))
+#生成num_of_player列的二維list
+matrx=[[0 for i in range(3)] for j in range(num_of_player)]
+#print(matrx)
+game_start(matrx,num_of_player)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
